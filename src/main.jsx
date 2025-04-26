3ec445feab7513c6bcc1d02af361179c4297a735
WADE <MEANTOINE12@GMAIL.COM>
2025-04-26 23:46:37 +0300

    resolved vercel error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {SearchProvider } from './context/searchContext.jsx'
-import { CollectionProvider } from './context/collectionContext.jsx';
 import routes from './routes.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Styles/Loader.css'
@@ -18,9 +17,7 @@ const router = createBrowserRouter(routes)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SearchProvider>
-      <CollectionProvider> 
         <RouterProvider router={router} />
-      </CollectionProvider>
     </SearchProvider>
   </StrictMode>
 );
